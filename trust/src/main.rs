57174471f3f7f029f8787b4712e1aecd0c3f9da6
liuyanan <oceanwave@yeah.net>
2019-05-19 20:15:21 +0800

    add parts of tcp

--- trust/src/main.rs
+++ trust/src/main.rs
@@ -1,8 +1,17 @@
-
-
 use std::io;
+use std::collections::HashMap;
+use std::net::Ipv4Addr;
+
+mod tcp;
+
+#![derive(Clone, Copy, Debug, Hash, Eq, PartialEq)]
+struct Quad {
+    src: (Ipv4Addr, u16),
+    dst: (Ipv4Addr, u16),
+}
 
 fn main() -> io::Result<()> {
+	let mut connections: HashMap<Quad, tcp::state> = Default::default();
 	let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
 	let mut buf = [0u8; 1504];
 	loop{
@@ -14,21 +23,24 @@ fn main() -> io::Result<()> {
 			continue;
 		}
 
+		let ip_hdr_sz = p.slice().len();
 		match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]){
-
-			 Ok(p) => {
-				 let src = p.source_addr();
-				 let dst = p.destination_addr();
-				 let proto = p.protocol(); // tcp udp etc... transmission protocol
-				 if proto != 0x06 {
+			 Ok(iph) => {
+				 let src = iph.source_addr();
+				 let dst = iph.destination_addr();
+				 if iph.protocol() != 0x06 {
 					// not tcp
 					continue;
 				 }
 				
-				 match etherparse::TcpHeaderSlice::from_slice(&buf[4+p.slice().len()..]) {
-					Ok(p) => {
-				        eprintln!("{} → {} {}b of tcp to port {}", 
-								src, dst, p.slice().len(), p.destination_port());
+				 match etherparse::TcpHeaderSlice::from_slice(&buf[4+iph.slice().len()..]) {
+					Ok(tcph) => {
+						let datai = 4 + iph.slice().len() + tcph.slice().len();
+					    connnections.entry(Quad {
+						    src: (src, tcph.source_port()),
+						    dst: (dst, tcph.destination_port()),
+					    }).or_default().on_packet(iph, tchp, &buf[datai..]);
+				        // (srcip, srcport, dstip, dstport)
 					},
 					Err(e) => {
 				        eprintln!("ignore weird tcp packet {:?}", e)
