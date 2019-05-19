7d7aecaac517d4d4b2a7764f606b78549b8c10c1
liuyanan <oceanwave@yeah.net>
2019-05-19 17:29:01 +0800

    创建虚拟网卡，接受网络包，解析ipv4包

---
+++ trust/README.md
@@ -0,0 +1,23 @@
+
+cargo r --release
+sudo setcap cap_net_admin=eip /home/liuyanan/my_work/rust_programming.git/trust/target/release/trust
+
+
+./target/release/trust
+
+ip addr
+
+sudo ip addr add 192.168.0.1/24 dev tun0
+
+
+sudo ip link set up dev tun0
+
+ping -I tun0 192.168.0.2
+
+
+#命令行版本的wireshark
+sudo tshark -i tun0
+
+
+
+pgrep -af python
---
+++ trust/src/main.rs
@@ -0,0 +1,29 @@
+
+
+use std::io;
+
+fn main() -> io::Result<()> {
+	let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
+	let mut buf = [0u8; 1504];
+	loop{
+	    let nbytes = nic.recv(&mut buf[..])?;
+		let flags = u16::from_be_bytes([buf[0], buf[1]]);
+		let proto = u16::from_be_bytes([buf[2], buf[3]]);
+		if proto != 0x0800 {
+			// no ipv4
+			continue;
+		}
+
+		match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]){
+			 Ok(p) => {
+				eprintln!("read {} bytes (flags:{:x}, proto:{:x}): {:x?}", 
+						nbytes-4, flags, proto, p]);
+			 },
+		     Err(e) => {
+				 eprintln!("ignore weird packet {:?}", e)
+			 }
+		}
+		
+	}
+	Ok(())
+}
