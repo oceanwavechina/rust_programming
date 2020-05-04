f6e78344909aa141b6b33f073c6683b25ced421e
liuyanan <oceanwavechina@outlook.com>
2020-05-04 16:03:22 +0800

    writing an os in Rust: A Minimal Rust Kernel

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -6,6 +6,8 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello World!";
+
 //
 // diverging function
 // https://doc.rust-lang.org/stable/rust-by-example/fn/diverging.html
@@ -18,5 +20,14 @@ fn panic(_info: &PanicInfo) -> ! {
 // disable the name mangling
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    loop {}
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for(i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop{}
 }
